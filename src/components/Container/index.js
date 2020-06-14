aa6a1898f16c896d47275eb642a4a3ae1d573986
iamcristos <nmereginivincent@gmail.com>
2020-06-14 15:59:52 +0100

    created card

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -10,6 +10,59 @@ export default function Container() {
             <Header />
             <ContainerWrapper margin="1.2rem">
                 <MarketDashboard />
+                <section>
+                    <div className="left">
+                    <div className="full-width">
+                    {/* <LineGraph /> */}
+                    </div>
+
+                    <div className="full-width">
+                    <div className="divided-width">
+                        {/* <TotalRevenue /> */}
+                    </div>
+
+                    <div className="divided-width">
+                        {/* <TotalTransactions /> */}
+                    </div>
+                    </div>
+
+                    <div className="full-width">
+                    {/* <MarketOverview /> */}
+                    </div>
+
+                    <div className="full-width">
+                    {/* <Invoices /> */}
+                    </div>
+
+                    <div className="full-width">
+                    <div className="divided-width">
+                        {/* <TotalRevenue /> */}
+                    </div>
+
+                    <div className="divided-width">
+                        {/* <TotalTransactions /> */}
+                    </div>
+                    </div>
+
+                    <div className="full-width">
+                    <div className="divided-width">
+                        {/* <RecentEvents /> */}
+                    </div>
+
+                    <div className="divided-width">
+                        {/* <RecentEvents /> */}
+                    </div>
+                    </div>
+                    </div>
+                    <div className="right">
+                    {/* <Radar />
+                    <AudienceMetric />
+                    <World />
+                    <World />
+                    <World />
+                    <World /> */}
+                    </div>
+                </section>
             </ContainerWrapper>
         </MainContainer>
     )
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 1rem 0;
+  background-color: #dbdbdb;
+  width: 100%;
+  .footer {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0 1rem;
+    color: #54575a;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <StyledFooter>
+      <div className="footer">
+        <p>@ {new Date().getFullYear()} Simple Dashboard FE Assessment</p>
+        <p>Designed By @Cristos</p>
+      </div>
+    </StyledFooter>
+  );
+};
+
+export default Footer;
